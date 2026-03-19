770fd8a9fba08a8365b2364c25da2b05669322e9
martina <martina.georgieva@students.finki.ukim.mk>
2026-03-19 19:16:38 +0100

    Redesign RegistrationSuccess page

--- frontend/react-app/src/pages/auth/RegistrationSuccess.jsx
+++ frontend/react-app/src/pages/auth/RegistrationSuccess.jsx
@@ -1,24 +1,25 @@
-import {Button} from "@/components/ui/button";
-import {Item, ItemActions, ItemContent, ItemDescription, ItemHeader, ItemTitle} from "@/components/ui/item"
-import {Link} from "react-router";
+import { Button } from "@/components/ui/button"
+import { Item, ItemActions, ItemContent, ItemDescription, ItemHeader, ItemTitle } from "@/components/ui/item"
+import { Link } from "react-router"
+import Header from '@/components/Header.jsx'
 
 export default function RegistrationSuccess() {
-    return (
-        <div className="flex w-full gap-6 justify-center">
-            <Item className='max-w-xl border-2 border-solid border-primary text-start'>
-                <ItemHeader className='font-bold text-xl text-center justify-center'>Account registration
-                    successful</ItemHeader>
-                <ItemContent className='basis-full text-sm/8'>
-                    <ItemTitle>Verify your account</ItemTitle>
-                    <ItemDescription >We have sent you a link for account verification on your email. Please verify your
-                        account in order to be able to use it.</ItemDescription>
-                </ItemContent>
-                <ItemActions>
-                    <Button asChild>
-                        <Link to={'/login'}>Login</Link>
-                    </Button>
-                </ItemActions>
-            </Item>
-        </div>
-    )
+  return (
+      <div className="flex w-full h-svh justify-center items-center">
+        <Item className="max-w-xl h-fit border-4 border-double rounded-xl shadow-lg shadow-primary/30 border-primary text-start gap-5 p-5">
+          <ItemHeader className="font-bold text-xl text-center justify-center">Account registration
+            successful</ItemHeader>
+          <ItemContent className="basis-full text-sm/8">
+            <ItemTitle>Verify your account</ItemTitle>
+            <ItemDescription>We have sent you a link for account verification on your email. Please verify your
+              account in order to be able to use it.</ItemDescription>
+          </ItemContent>
+          <ItemActions>
+            <Button asChild>
+              <Link to={'/login'}>Login</Link>
+            </Button>
+          </ItemActions>
+        </Item>
+      </div>
+  )
 }
